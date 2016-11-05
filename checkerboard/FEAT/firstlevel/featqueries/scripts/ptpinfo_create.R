be3dfec38e18735d891c9a268caf2f7bd4b4d507
Peder Isager <pederisager@gmail.com>
2016-11-05 14:35:13 +0100

    Created subject_info folder, and associated R project.

--- checkerboard/FEAT/firstlevel/featqueries/scripts/ptpinfo_create.R
+++ checkerboard/FEAT/firstlevel/featqueries/scripts/ptpinfo_create.R
@@ -1,3 +1,24 @@
 ptpinfo <- read.table("druglist.txt")
 
 ptpinfo$sex <- ifelse(ptpinfo$ptp > 536, "female", "male")
+
+# In
+
+subjectinfo = NULL
+
+
+
+subjectinfo <- data.frame(
+  subject = c(501,502, 504,505,506, 509:513, 515:529, 531:536, 601:616, 618:632),
+  session = c(1,2),
+  drug = c(
+    "pla", "mor", "pla", "mor", "pla", "mor", "mor", "pla", "pla", "mor", "mor", "pla", "pla", "mor", "mor", "pla", "pla",
+    "mor", "mor", "pla", "mor", "pla", "pla", "mor", "mor", "pla", "pla", "mor", "mor", "pla", "pla", "mor", "mor", "pla",
+    "pla", "mor", "mor", "pla", "pla", "mor", "mor", "pla", "pla", "mor", "mor", "pla", "pla", "mor", "mor", "pla", "pla",
+    "mor", "pla", "mor", "pla", "mor", "mor", "pla", "pla", "mor", "pla", "mor", "mor", "pla", "pla", "mor", "mor", "pla",
+    "pla", "mor", "mor", "pla", "pla", "mor", "mor", "pla", "pla", "mor", "mor", "pla", "pla", "mor", "mor", "pla", "pla",
+    "mor", "mor", "pla", "pla", "mor", "mor", "pla", "pla", "mor", "pla", "mor", "mor", "pla", "mor", "pla", "mor", "pla",
+    "pla", "mor", "mor", "pla", "pla", "mor", "mor", "pla", "pla", "mor", "pla", "mor", "mor", "pla", "mor", "pla", "pla",
+    "mor", "pla", "mor", "mor", "pla")
+  gender = c(ifelse(subjectinfo$subject < 600, "male", "female"))
+  )
